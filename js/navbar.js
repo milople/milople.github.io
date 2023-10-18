ebe95308c4c4db4d1b67b30d7b9ca2650481c050
Manish Milople <manishgour@milople.com>
2023-10-18 18:50:42 +0530

    added files

--- js/navbar.js
+++ js/navbar.js
@@ -1,47 +1,12 @@
+$(document).ready(function($){
+$("#search-icon").click(function() {
+  $(".nav").toggleClass("search");
+  $(".nav").toggleClass("no-search");
+  $(".search-input").toggleClass("search-active");
+});
 
-(function($) { "use strict";
-
-  $(function() {
-    var header = $(".start-style");
-    $(window).scroll(function() {    
-      var scroll = $(window).scrollTop();
-    
-      if (scroll >= 10) {
-        header.removeClass('start-style').addClass("scroll-on");
-      } else {
-        header.removeClass("scroll-on").addClass('start-style');
-      }
-    });
-  });   
-    
-  //Animation
-  
-  $(document).ready(function() {
-    $('body.hero-anime').removeClass('hero-anime');
-  });
-
-  //Menu On Hover
-    
-  $('body').on('mouseenter mouseleave','.nav-item',function(e){
-      if ($(window).width() > 750) {
-        var _d=$(e.target).closest('.nav-item');_d.addClass('show');
-        setTimeout(function(){
-        _d[_d.is(':hover')?'addClass':'removeClass']('show');
-        },1);
-      }
-  }); 
-  
-  //Switch light/dark
-  
-  $("#switch").on('click', function () {
-    if ($("body").hasClass("dark")) {
-      $("body").removeClass("dark");
-      $("#switch").removeClass("switched");
-    }
-    else {
-      $("body").addClass("dark");
-      $("#switch").addClass("switched");
-    }
-  });  
-  
-  })(jQuery); 
+$('.menu-toggle').click(function(){
+   $(".nav").toggleClass("mobile-nav");
+   $(this).toggleClass("is-active");
+});
+});
